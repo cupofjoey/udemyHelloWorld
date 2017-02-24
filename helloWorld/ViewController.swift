9570a43cb9c9112907180892b015ad219d3f216c
Joe Hill <joe@jkh3.com>
2017-02-23 21:48:20 -0500

    First Commit for simple 'Hello World' iOS app

---
+++ helloWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  helloWorld
+//
+//  Created by BishopHill on 2/23/17.
+//  Copyright © 2017 Dumb Unicorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
